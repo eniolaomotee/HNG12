547e7f7d9350df8d629749eba7bea4e1ecc4c693
eniolaomotee <eniolaomotomi@gmail.com>
2025-01-30 00:17:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def get_info():
+    return {
+        "email": "eniolaomotomi@gmail.com",
+        "current_datetime": datetime.utcnow().isoformat() + "Z",
+        "github_url": "https://github.com/eniolaomotee/HNG12_Internship"  # Replace with your GitHub repo URL
+    }
